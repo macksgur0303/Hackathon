d1d3ae59736418c7ed94e72e31d5e46d6ff83b87
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-08-30 03:47:20 +0900

    Add files via upload

---
+++ 2023.08.29/update2/myapplication/HomeActivity_2.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+
+        findViewById(R.id.btn_home_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity_2.this, HomeActivity_2.class));
+            }
+        });
+    }
+}
